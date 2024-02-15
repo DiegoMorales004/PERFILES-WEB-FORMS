6a0b173ca8218558ef8a343dc7b25bc07d939c13
DiegoMorales004 <diegomoralesss007@gmail.com>
2024-02-15 11:47:00 -0600

    Se agrego metodo eliminar para departamentos

--- PERFILES.DataLayer/DepartmentDL.cs
+++ PERFILES.DataLayer/DepartmentDL.cs
@@ -14,6 +14,70 @@ namespace PERFILES.DataLayer
     internal class DepartmentDL
     {
 
+        //Create department
+        public bool CreateDepartment(Department department)
+        {
+            using(SqlConnection localConnection =  new SqlConnection(Connection.connection))
+            {
+
+                SqlCommand cmd = new SqlCommand("SP_CreateDepartment", localConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                AddWithValue(ref cmd, department, false);
+
+                try
+                {
+
+                    bool Created = CheckRowsAffected(localConnection, ref cmd);
+                    return Created;
+
+                }catch(Exception ex)
+                {
+                    throw ex;
+                }
+
+            }
+        }
+
+        //Get one department
+        public Department GetDepartment(int id)
+        {
+            Department Department = new Department();
+
+            using(SqlConnection localConnection = new SqlConnection(Connection.connection))
+            {
+                SqlCommand cmd = new SqlCommand("SP_ReadOneDepartment", localConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", id);
+
+                try
+                {
+                    
+                    localConnection.Open();
+
+                    using(SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            Department.Id = Convert.ToInt32(reader["id"].ToString());
+                            Department.Name = reader["name"].ToString();
+                            Department.Description = reader["description"].ToString();
+
+                        }
+                    }
+
+                    return Department;
+
+                }
+                catch(Exception e)
+                {
+                    throw e;
+                }
+
+            }
+
+        }
+
         //Get all departments
         public List<Department> DepartmentsList()
         {
@@ -22,8 +86,8 @@ namespace PERFILES.DataLayer
 
             using (SqlConnection localConnection = new SqlConnection(Connection.connection))
             {
-                SqlCommand cmd = new SqlCommand("exec SP_ReadDepartment", localConnection);
-                cmd.CommandType = CommandType.Text;
+                SqlCommand cmd = new SqlCommand("SP_ReadDepartment", localConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
                 try {
                     localConnection.Open();
                     using(SqlDataReader reader = cmd.ExecuteReader())
@@ -47,6 +111,74 @@ namespace PERFILES.DataLayer
                 }
             }
 
+        }
+
+        //Update department
+        public bool UpdateDepartment(Department department)
+        {
+            using(SqlConnection localConnection = new SqlConnection())
+            {
+                
+                SqlCommand cmd = new SqlCommand("SP_UpdateDepartment", localConnection);
+
+                AddWithValue(ref cmd, department, true);
+
+                try
+                {
+
+                    bool Updated = CheckRowsAffected(localConnection, ref cmd);
+                    return Updated;
+
+                }catch(Exception e)
+                {
+                    throw e;
+                }
+
+            }
+        }
+
+        //Delete 
+        public bool DeleteDeparment(int id)
+        {
+            using (SqlConnection localConnection = new SqlConnection(Connection.connection))
+            {
+                SqlCommand cmd = new SqlCommand("SP_DeleteDepartment", localConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@id", id);
+
+                try
+                {
+                    bool Deleted = CheckRowsAffected(localConnection, ref cmd);
+                    return Deleted;
+                }catch(Exception ex)
+                {
+                    throw ex;
+                }
+
+            }
+        }
+
+        // Process of adding information to the stored procedure
+        private void AddWithValue(ref SqlCommand cmd, Department department, bool update)
+        {
+
+            if (update) cmd.Parameters.AddWithValue("@id", department.Id);
+
+            cmd.Parameters.AddWithValue("@name", department.Name);
+            cmd.Parameters.AddWithValue("@description", department.Description);
+
+        }
+
+        //Open the connection to the database and verify if the command was executed successfully
+        public static bool CheckRowsAffected(SqlConnection localConnection, ref SqlCommand cmd)
+        {
+            localConnection.Open();
+
+            int RowsAffected = cmd.ExecuteNonQuery();
+            if (RowsAffected > 0) return true;
+
+            return false;
         }
 
     }
